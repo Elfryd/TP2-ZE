9ae52bb87c80f29fafa762839aaf08822d7a5871
geoffrey_polan <geoffrey_polan@ens.univ-artois.fr>
2017-11-22 10:07:50 +0100

    MAJ de la branche TP3-BZ

--- src/Entity/Player.php
+++ src/Entity/Player.php
@@ -8,6 +8,8 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
 /**
  * Class Player
  * @package App\Entity
@@ -15,8 +17,92 @@ namespace App\Entity;
  */
 class Player
 {
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
     protected $id;
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=40)
+     */
     protected $name;
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     */
     protected $healthPoint = 100;
+    /**
+     * @var Weapon
+     * @ORM\ManyToOne(targetEntity="weapon")
+     */
     protected $currentWeapon;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHealthPoint()
+    {
+        return $this->healthPoint;
+    }
+
+    /**
+     * @param int $healthPoint
+     */
+    public function setHealthPoint($healthPoint)
+    {
+        $this->healthPoint = $healthPoint;
+    }
+
+    /**
+     * @return Weapon
+     */
+    public function getCurrentWeapon()
+    {
+        return $this->currentWeapon;
+    }
+
+    /**
+     * @param Weapon $currentWeapon
+     */
+    public function setCurrentWeapon($currentWeapon)
+    {
+        $this->currentWeapon = $currentWeapon;
+    }
+
+
 }
--- src/Entity/Weapon.php
+++ src/Entity/Weapon.php
@@ -8,8 +8,99 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * Class Weapon
+ * @package App\Entity
+ * @ORM\Entity
+ */
 class Weapon
 {
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=40)
+     */
+    protected $name;
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     */
+    protected $damage;
+    /**
+     * @var float
+     * @ORM\Column(type="decimal")
+     */
+    protected $damageDistanceCoeff;
+    /**
+     * @var int
+     * @ORM\Column(type="integer")
+     */
+    protected $fireRate;
+
+    /**
+     * Weapon constructor.
+     * @param string $name
+     * @param int $damage
+     * @param float $damageDistanceCoeff
+     * @param int $fireRate
+     */
+    public function __construct($name, $damage, $damageDistanceCoeff, $fireRate)
+    {
+        $this->name = $name;
+        $this->damage = $damage;
+        $this->damageDistanceCoeff = $damageDistanceCoeff;
+        $this->fireRate = $fireRate;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDamage()
+    {
+        return $this->damage;
+    }
+
+    /**
+     * @return float
+     */
+    public function getDamageDistanceCoeff()
+    {
+        return $this->damageDistanceCoeff;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFireRate()
+    {
+        return $this->fireRate;
+    }
+
+
 
 }
